eb94c38a974dc4c622d39e26e1d45b55f57c3327
stohelitskii <andrenord282@gmail.com>
2024-04-26 15:56:06 +0500

    feat: добавил StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-// import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App/App.tsx';
 import 'config/configureMobX.ts';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
+  <StrictMode> 
     <App />
-  // </StrictMode>,
+ </StrictMode>,
 );
